89c94935f7ae7550d2af8f66423a8a4f62a2d41b
lnbEli <137424458+lnbEli@users.noreply.github.com>
2025-07-15 13:31:51 +0200

    create helper function to populate gameboard grid

---
+++ initialiseGrid.js
@@ -0,0 +1,11 @@
+function initialiseGrid(gridSize) {
+  const arrayY = [];
+  for (let i = 0; i < gridSize; i++) {
+    const arrayX = [];
+    for (let j = 0; j < gridSize; j++) {
+      arrayX.push(0);
+    }
+    arrayY.push(arrayX);
+  }
+  return arrayY;
+}
