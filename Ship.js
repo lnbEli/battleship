dec49f2cefddeeaa42cd6630a6869db386d6a12c
lnbEli <137424458+lnbEli@users.noreply.github.com>
2025-07-16 19:50:51 +0200

    Refactored ship class and test

--- Ship.js
+++ Ship.js
@@ -1,18 +1,32 @@
 export default class Ship {
-  constructor(length, timesHit = 0) {
-    if (typeof length !== "number" || !Number.isFinite(length) || length <= 0) {
-      throw new Error("Length must be positive number");
-    }
+  constructor(typeOfShip) {
     if (
-      typeof timesHit !== "number" ||
-      !Number.isFinite(timesHit) ||
-      timesHit < 0
+      typeOfShip !== "Carrier" &&
+      typeOfShip !== "Battleship" &&
+      typeOfShip !== "Cruiser" &&
+      typeOfShip !== "Submarine" &&
+      typeOfShip !== "Destroyer"
     ) {
-      throw new Error("Times hit input must be positive number");
+      throw new Error(
+        `${typeOfShip} is not permitted type. Type must be string and one of the following: Carrier, Battleship, Cruiser, Submarine, Destroyer`
+      );
     }
-    this.length = length;
-    this.timesHit = timesHit;
+
+    // Define lengths for each type of ship
+    const lengths = {
+      Carrier: 5,
+      Battleship: 4,
+      Cruiser: 3,
+      Submarine: 3,
+      Destroyer: 2,
+    };
+
+    this.timesHit = 0;
+    this.typeOfShip = typeOfShip;
+    // Assign the length based on the type of ship
+    this.length = lengths[this.typeOfShip];
   }
+
   hit() {
     this.timesHit++;
   }
--- Ship.test.js
+++ Ship.test.js
@@ -5,67 +5,69 @@ describe("Ship Class", () => {
     expect(Ship).toBeDefined();
   });
 
-  //Don't need to test Class but do test instances???
-  //Class constructor is not accessed by user....
   test("check is Class", () => {
-    const instance = new Ship(3);
+    const instance = new Ship("Carrier");
     expect(instance).toBeInstanceOf(Ship);
   });
 
-  test("check error thrown if string value inputted (1)", () => {
-    expect(() => new Ship("2")).toThrow("Length must be positive number");
+  test("check error thrown if incorrect type of ship entered", () => {
+    expect(() => new Ship("Carr")).toThrow(Error);
   });
 
-  test("check error thrown if string value inputted (2)", () => {
-    expect(() => new Ship(2, "4")).toThrow(
-      "Times hit input must be positive number"
-    );
+  test("check error thrown if no ship typ entered", () => {
+    expect(() => new Ship()).toThrow(Error);
   });
 
-  test("check error thrown if string value inputted (3)", () => {
-    expect(() => new Ship(-2)).toThrow("Length must be positive number");
-  });
-
-  test("check error thrown if string value inputted (4)", () => {
-    expect(() => new Ship(2, -4)).toThrow(
-      "Times hit input must be positive number"
-    );
-  });
   test("check Hit function/method exists", () => {
-    const instance = new Ship(5);
+    const instance = new Ship("Battleship");
     expect(instance.hit).toBeInstanceOf(Function);
   });
+
   test("check Hit function increases number of hits on ship (1)", () => {
-    const instance = new Ship(4);
+    const instance = new Ship("Battleship");
     instance.hit();
     expect(instance.timesHit).toBe(1);
+    instance.hit();
+    expect(instance.timesHit).toBe(2);
   });
 
   test("check Hit function increases number of hits on ship (2)", () => {
-    const instance = new Ship(4, 3);
+    const instance = new Ship("Submarine");
     instance.hit();
-    expect(instance.timesHit).toBe(4);
+    instance.hit();
+    instance.hit();
+    expect(instance.timesHit).toBe(3);
   });
 
   test("isSunk function/method exists", () => {
-    const instance = new Ship(5);
+    const instance = new Ship("Submarine");
     expect(instance.isSunk).toBeInstanceOf(Function);
   });
 
   test("check isSunk function (1)", () => {
-    const instance = new Ship(4, 4);
+    const instance = new Ship("Submarine");
+    instance.timesHit = instance.length;
     const isSunk = instance.isSunk();
     expect(isSunk).toBe(true);
   });
 
   test("check isSunk function (2)", () => {
-    const instance = new Ship(5, 4);
+    const instance = new Ship("Submarine");
+    instance.timesHit = instance.length - 1;
     const isSunk = instance.isSunk();
     expect(isSunk).toBe(false);
   });
 
   test("check isSunk function (3)", () => {
-    const instance = new Ship(2);
+    const instance = new Ship("Destroyer");
+    instance.timesHit = instance.length + 1;
+    const isSunk = instance.isSunk();
+    expect(isSunk).toBe(true);
+  });
+
+  test("check isSunk function (4)", () => {
+    const instance = new Ship("Destroyer");
+    instance.timesHit = 1;
     const isSunk = instance.isSunk();
     expect(isSunk).toBe(false);
   });
